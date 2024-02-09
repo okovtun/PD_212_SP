8e622a47a0daf4cbf5049179fed068f805cbc627
Clayman <Clayman@DESKTOP-DM8E77U>
2024-02-09 17:21:48 +0300

    HomeWork

--- IntroductionToSP/Processes/Form1.Designer.cs
+++ IntroductionToSP/Processes/Form1.Designer.cs
@@ -94,6 +94,7 @@
 			this.Controls.Add(this.richTextBoxProcessName);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
--- IntroductionToSP/Processes/Form1.cs
+++ IntroductionToSP/Processes/Form1.cs
@@ -18,6 +18,11 @@ namespace Processes
 			richTextBoxProcessName.Text = "notepad";
 			InitProcess();
 		}
+		//void Form1_Closing(object sender, CancelEventArgs e)
+		//{
+		//	myProcess.CloseMainWindow();
+		//	myProcess.Close();
+		//}
 		void InitProcess()
 		{
 			AllignText();
@@ -33,12 +38,34 @@ namespace Processes
 		{
 			InitProcess();
 			myProcess.Start();
+			Info();
 		}
 
 		private void buttonStop_Click(object sender, EventArgs e)
 		{
 			myProcess.CloseMainWindow();	//закрывает процесс
 			myProcess.Close();		//освобождает ресурсы, занимаемые процессом
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			myProcess.CloseMainWindow();
+			myProcess.Close();
+		}
+		void Info()
+		{
+			labelProcessInfo.Text = "Process info:\n";
+			labelProcessInfo.Text += $"PID:				{myProcess.Id}\n";
+			labelProcessInfo.Text += $"Base priority:	{myProcess.BasePriority}\n";
+			labelProcessInfo.Text += $"Priority class:	{myProcess.PriorityClass}\n";
+			labelProcessInfo.Text += $"Start time:		{myProcess.StartTime}\n";
+			labelProcessInfo.Text += $"Total CPU time:	{myProcess.TotalProcessorTime}\n";
+			labelProcessInfo.Text += $"User  CPU time:	{myProcess.UserProcessorTime}\n";
+			labelProcessInfo.Text += $"User  CPU time:	{myProcess.UserProcessorTime}\n";
+			labelProcessInfo.Text += $"Session ID:		{myProcess.SessionId}\n";
+			labelProcessInfo.Text += $"Name:			{myProcess.ProcessName}\n";
+			labelProcessInfo.Text += $"Affinity:		{myProcess.ProcessorAffinity}\n";
+			labelProcessInfo.Text += $"Threads:			{myProcess.Threads.Count}\n";
 		}
 	}
 }
